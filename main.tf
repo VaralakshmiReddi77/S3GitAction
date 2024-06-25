142ede6883ad829b3024b99bf632bc5e30542f18
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-25 14:24:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.64.0"
-    }
-  }
-}
 resource "aws_s3_bucket" "mybucket-elabel-001" {
   bucket = "mybucket-elabel-001"
 
