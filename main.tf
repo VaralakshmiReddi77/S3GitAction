81dd2ee5081cbc809e578f90dcaf72dd2b717a7f
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-25 17:21:17 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
-# main.tf
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-example-bucket1"
-    tags = {
-      Name        = "MyS3Bucket1"
-    }
+  bucket = "example"
 }
-resource "aws_s3_bucket_acl" "example" {
+
+resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
-  acl    = "public-read-write"
+
+  block_public_policy     = false // This is default, so you can probably remove this line
+  restrict_public_buckets = false // same as above
+  block_public_acls       = true 
+  ignore_public_acls      = true 
 }
 
