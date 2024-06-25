cb835fdcfd7984631f9acbf70d787f75998f6abc
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-25 16:38:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,28 @@
-provider "aws" {
-  region = "us-east-1"
+# main.tf
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket"
+    tags = {
+      Name        = "MyS3Bucket"
+      Environment = "Production"
+    }
 }
-resource "aws_s3_bucket" "mybucket-elabel-001" {
-  bucket = "mybucket-elabel-001"
-
-  tags = {
-    Name        = "My bucket"
-  }
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
 }
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.my_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-tf-log-bucket"
+    tags = {
+      Name        = "MyLogBucket"
+      Environment = "Production"
+    }
+}
+resource "aws_s3_bucket_acl" "log_bucket_acl" {
+  bucket = aws_s3_bucket.log_bucket.id
+  acl    = "log-delivery-write"
+}
+resource "aws_s3_bucket_logging" "example" {
+  bucket        = aws_s3_bucket.example.id
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
 }
