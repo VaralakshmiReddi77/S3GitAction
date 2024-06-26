2e96f225549e3c734593eb74366c603f1c0e0598
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-26 13:37:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-example-bucket1"
+  bucket = "my-tf-example-bucket"
     tags = {
       Name        = "MyS3Bucket"
       Environment = "Dev"
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_acl" "example" {
   acl    = "private"
 }
 resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my-tf-log-bucket1"
+  bucket = "my-tf-log-bucket"
     tags = {
       Name        = "MyLogBucket"
       Environment = "Dev"
