a448ad396e6d9909e7aa9ae865cb3bed799949cc
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-26 13:49:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,14 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-example-bucket"
-    tags = {
-      Name        = "MyS3Bucket"
-      Environment = "Dev"
-    }
+# main.tf
+resource "aws_s3_bucket" "my_bucket" {
+  bucket  = "my-unique-bucket-name"
+  tags    = {
+  Name           = "MyS3Bucket"
+  Environment    = "Production"
+  }
 }
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
-}
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my-tf-log-bucket"
-    tags = {
-      Name        = "MyLogBucket"
-      Environment = "Dev"
-    }
-}
-resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.log_bucket.id
-  acl    = "log-delivery-write"
-}
-resource "aws_s3_bucket_logging" "example" {
-  bucket        = aws_s3_bucket.example.id
-  target_bucket = aws_s3_bucket.log_bucket.id
-  target_prefix = "log/"
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
