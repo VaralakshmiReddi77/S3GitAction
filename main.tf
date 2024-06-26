3251f0fc7d7bac68d207377d310cd6728b06d97f
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-26 14:03:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
-# main.tf
-resource "aws_s3_bucket" "my_bucket2" {
-  bucket  = "my-unique-bucket-name2"
-  tags    = {
-  Name           = "MyS3Bucket2"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+}
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.example.arn}/*",
+    ]
   }
 }
