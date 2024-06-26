4e84cb4890d15dc506cd0118d823722bd97b81a6
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-26 14:26:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,50 +1,16 @@
-resource "aws_quicksight_dashboard" "example" {
-  dashboard_id        = "example-id"
-  name                = "example-name"
-  version_description = "version"
-  definition {
-    data_set_identifiers_declarations {
-      data_set_arn = aws_quicksight_data_set.dataset.arn
-      identifier   = "1"
-    }
-    sheets {
-      title    = "Example"
-      sheet_id = "Example1"
-      visuals {
-        line_chart_visual {
-          visual_id = "LineChart"
-          title {
-            format_text {
-              plain_text = "Line Chart Example"
-            }
-          }
-          chart_configuration {
-            field_wells {
-              line_chart_aggregated_field_wells {
-                category {
-                  categorical_dimension_field {
-                    field_id = "1"
-                    column {
-                      data_set_identifier = "1"
-                      column_name         = "Column1"
-                    }
-                  }
-                }
-                values {
-                  categorical_measure_field {
-                    field_id = "2"
-                    column {
-                      data_set_identifier = "1"
-                      column_name         = "Column1"
-                    }
-                    aggregation_function = "COUNT"
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+resource "aws_dynamodb_table" "this" {
+  name           = var.name
+  billing_mode   = var.billing_mode
+  read_capacity  = var.read_capacity
+  write_capacity = var.write_capacity
+  hash_key       = var.hash_key
+  server_side_encryption {
+    enabled = var.encryption
   }
+  attribute {
+    name = var.hash_key
+    type = "S"
+  }
+
+  tags = merge({ Name : var.name }, var.tags)
 }
