0fa64d6755c0052f7d6df7f175eccb16184b9702
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-26 13:52:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # main.tf
-resource "aws_s3_bucket" "my_bucket1" {
+resource "aws_s3_bucket1" "my_bucket1" {
   bucket  = "my-unique-bucket-name1"
   tags    = {
   Name           = "MyS3Bucket1"
   }
 }
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.my_bucket1.id
+resource "aws_s3_bucket1_versioning" "versioning_example" {
+  bucket = aws_s3_bucket1.my_bucket1.id
   versioning_configuration {
     status = "Enabled"
   }
