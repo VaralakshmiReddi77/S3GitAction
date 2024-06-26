475721dbf3c08b607045594c29273a6165c48346
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-26 14:21:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,3 +26,17 @@ resource "aws_s3_bucket_logging" "example" {
   target_bucket = aws_s3_bucket.log_bucket.id
   target_prefix = "log/"
 }
+resource "aws_quicksight_dashboard" "example" {
+  dashboard_id        = "example-id"
+  name                = "example-name"
+  version_description = "version"
+  source_entity {
+    source_template {
+      arn = aws_quicksight_template.source.arn
+      data_set_references {
+        data_set_arn         = aws_quicksight_data_set.dataset.arn
+        data_set_placeholder = "1"
+      }
+    }
+  }
+}
