6412875299217656bafe0d4b6d4c16f971315655
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-26 14:45:48 +0200

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "The ARN of the table created"
-  value       = aws_dynamodb_table.this.arn
-}
-
-output "name" {
-  description = "The name of the table created"
-  value       = aws_dynamodb_table.this.id
-}
