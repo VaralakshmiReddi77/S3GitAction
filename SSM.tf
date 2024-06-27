b2d9db89bbe1542965e5c4aa31a3972d73bdb2aa
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-27 16:14:10 +0200

    Update SSM.tf

--- SSM.tf
+++ SSM.tf
@@ -1,13 +1,13 @@
-resource "awscc_ssm_parameter" "example1" {
-  name            = "command1"
+resource "awscc_ssm_parameter" "configcred" {
+  name            = "DBPassword"
   type            = "String"
-   value = "null"
+  value = "null"
 
   lifecycle {
     ignore_changes        = all
   }
 }
 
-data "aws_ssm_parameters_by_path" "example1" {
-  path = "/command1"
+data "aws_ssm_parameters_by_path" "configcred" {
+  path = "/DBPassword"
 }
