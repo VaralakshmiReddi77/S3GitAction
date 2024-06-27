eebdced801eb6a6a21d05608a149b0a7045fae45
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-27 13:57:58 +0200

    Update SSM.tf

--- SSM.tf
+++ SSM.tf
@@ -5,23 +5,3 @@ resource "awscc_ssm_parameter" "example" {
   description     = "SSM Parameter for running date command."
   allowed_pattern = "^[a-zA-Z]{1,10}$"
 }
-module "store_write" {
-  source  = "cloudposse/ssm-parameter-store/aws"
-
-  parameter_write = [
-    {
-      name        = "/cp/prod/app/database/master_password"
-      value       = "password1"
-      type        = "String"
-      description = "Production database master password"
-    }
-  ]
-
-  tags = {
-    ManagedBy = "Terraform"
-  }
-}
-module "store_read" {
-  source  = "cloudposse/ssm-parameter-store/aws"
-  parameter_read = ["/cp/prod/app/database/master_password"]
-}
