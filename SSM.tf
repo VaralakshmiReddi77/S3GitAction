006da90a28801f6785f312d93c452ce7dfe08f8b
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-27 16:33:32 +0200

    Update SSM.tf

--- SSM.tf
+++ SSM.tf
@@ -1,25 +1,6 @@
-resource "aws_ssm_parameter" "bla" {
-  for_each = { for x in var.ssm_params : x.name => x }
-
-  name      = each.value.name
+resource "aws_ssm_parameter" "SampleSSMUpdate" {
+  name      = "Test"
   type      = "SecureString"
-  value     = each.value.value
+  value     = "Varalakshmi"
   overwrite = true
-}
-
-variable "ssm_params" {
-  type = map(object({
-    name  = string
-    value = string
-  }))
-  default = {
-    name : {
-      name  = "pesho"
-      value = "blaa22"
-    },
-    blaa : {
-      name  = "gosho"
-      value = "blaa11"
-    }
-  }
 }
