7be3576c01fbb366f18b070647b25b9ff36d75ba
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-27 15:31:09 +0200

    Update SSM.tf

--- SSM.tf
+++ SSM.tf
@@ -4,15 +4,10 @@ resource "awscc_ssm_parameter" "example1" {
   value           = "date"
   description     = "SSM Parameter for running date command."
   allowed_pattern = "^[a-zA-Z]{1,10}$"
-  overwrite = false
-
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
+    lifecycle {
+    ignore_changes = [value]
   }
 }
-
 data "aws_ssm_parameters_by_path" "example1" {
   path = "/command1"
 }
