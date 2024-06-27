1efbd5c6277dc880ad14a039845877aaadf638c2
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-27 16:13:04 +0200

    Update SSM.tf

--- SSM.tf
+++ SSM.tf
@@ -1,25 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.55.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
 resource "awscc_ssm_parameter" "example1" {
   name            = "command1"
   type            = "String"
-  value           = "Varalakshmi"
-  description     = "SSM Parameter for running date command."
-  allowed_pattern = "^[a-zA-Z]{1,10}$"
-    lifecycle {
-    ignore_changes = [value, tags]
+   value = "null"
+
+  lifecycle {
+    ignore_changes        = all
   }
 }
+
 data "aws_ssm_parameters_by_path" "example1" {
   path = "/command1"
 }
