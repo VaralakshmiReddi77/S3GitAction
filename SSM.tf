dd6e5f647e39267736ec7522678c3112be0664ca
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-27 13:55:20 +0200

    Update SSM.tf

--- SSM.tf
+++ SSM.tf
@@ -7,15 +7,12 @@ resource "awscc_ssm_parameter" "example" {
 }
 module "store_write" {
   source  = "cloudposse/ssm-parameter-store/aws"
-  # Cloud Posse recommends pinning every module to a specific version
-  # version = "x.x.x"
 
   parameter_write = [
     {
       name        = "/cp/prod/app/database/master_password"
       value       = "password1"
       type        = "String"
-    #  overwrite   = "true"
       description = "Production database master password"
     }
   ]
@@ -26,8 +23,5 @@ module "store_write" {
 }
 module "store_read" {
   source  = "cloudposse/ssm-parameter-store/aws"
-  # Cloud Posse recommends pinning every module to a specific version
-  # version = "x.x.x"
-
   parameter_read = ["/cp/prod/app/database/master_password"]
 }
