eded827b8d81badd20bf4bf8ea8ef6f07d0654f1
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-27 16:38:35 +0200

    Update SSM.tf

--- SSM.tf
+++ SSM.tf
@@ -4,3 +4,13 @@ resource "aws_ssm_parameter" "SampleSSMUpdate" {
   value     = "Taosif"
   overwrite = true
 }
+
+resource "awscc_ssm_parameter" "DBPassword" {
+  name            = "DBPassword"
+  type            = "String"
+  value           = "Varalakshmi"
+  description     = "SSM Parameter for running date command."
+  allowed_pattern = "^[a-zA-Z]{1,10}$"
+  overwrite = true
+}
+
