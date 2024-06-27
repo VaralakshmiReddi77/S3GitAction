f53197d33f17da328315c00df91343a02cca66aa
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-27 14:59:26 +0200

    Update SSM.tf

--- SSM.tf
+++ SSM.tf
@@ -1,10 +1,10 @@
-resource "awscc_ssm_parameter" "example" {
-  name            = "command"
+resource "awscc_ssm_parameter" "example1" {
+  name            = "command1"
   type            = "String"
   value           = "date"
   description     = "SSM Parameter for running date command."
   allowed_pattern = "^[a-zA-Z]{1,10}$"
 }
-data "aws_ssm_parameters_by_path" "example" {
-  path = "/command"
+data "aws_ssm_parameters_by_path" "example1" {
+  path = "/command1"
 }
