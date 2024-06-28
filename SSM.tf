c48b95d3668ac72dfed155bc9e17716944953439
VaralakshmiReddi77 <varalakshmi.reddi.ext@bayer.com>
2024-06-28 14:58:20 +0200

    Update SSM.tf

--- SSM.tf
+++ SSM.tf
@@ -1,13 +1,13 @@
 resource "aws_ssm_parameter" "/systems/fsystem01/config" {
   name      = "/systems/fsystem01/config"
   type      = "String"
-  value     = "{"url" : "https://www.website1.com"}"
+  value     = "https://www.website1.com"
   overwrite = true
 }
 
 resource "aws_ssm_parameter" "/systems/fsystem02/config" {
   name            = "/systems/fsystem02/config"
   type            = "String"
-  value           = "{"url" : "https://www.ESL.com"}"
+  value           = "https://www.ESL.com"
   overwrite = true
 }
